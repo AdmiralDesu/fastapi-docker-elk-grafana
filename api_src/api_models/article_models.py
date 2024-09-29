5d274ac7538f6111631f39cb06a7764327b27d26
AdmiralDesu <skyrim.men024@gmail.com>
2024-09-29 15:43:11 +0300

    1. Модели вынесены отдельно от api для решения проблемы коллизий миграций 2. Переделана структура проекта 3. Создана директория и модели для article_api 4. Создана новая миграция 5. Исправлены Dockerfiles

---
+++ api_src/api_models/article_models.py
@@ -0,0 +1,52 @@
+import datetime
+from typing import Annotated
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    Text,
+    UUID,
+    TEXT,
+    ForeignKey,
+    text
+)
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+from base_model import Base
+
+created_at = Annotated[datetime.datetime, mapped_column(server_default=text("now()"), index=True)]
+updated_at = Annotated[datetime.datetime, mapped_column(
+        server_default=text("now()"),
+        onupdate=datetime.datetime.now(),
+    )]
+
+
+class Article(Base):
+    __tablename__ = 'articles'
+
+    id: Mapped[str] = Column(UUID, primary_key=True, index=True, server_default=text("gen_random_uuid()"), unique=True)
+    title = Column(Text, nullable=False, index=True)
+    content = Column(Text, nullable=False, index=True)
+    folder_id: Mapped[str] = Column(UUID, ForeignKey('app.files_tree.id'), nullable=False)
+    icon_url: Mapped[str] = Column(Text, nullable=True)
+    article_type: Mapped[str] = Column(ForeignKey("app.articles_types.id"), nullable=False)
+    created_at: Mapped[created_at]
+    created_by: Mapped[str] = Column(Text, nullable=False, index=True)
+    updated_at: Mapped[updated_at]
+
+    __table_args__ = (
+        {"schema": "app"},
+    )
+
+
+class ArticleType(Base):
+    __tablename__ = 'articles_types'
+
+    id: Mapped[str] = Column(UUID, primary_key=True, index=True, server_default=text("gen_random_uuid()"), unique=True)
+    name: Mapped[str] = Column(Text, nullable=False, index=True)
+    description: Mapped[str] = Column(Text, nullable=False, index=True)
+
+    __table_args__ = (
+        {"schema": "app"},
+    )
+
